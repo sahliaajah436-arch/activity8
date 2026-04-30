c167da9ef533cee8dbd6afeaa7dceaffc3f9ecb7
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-30 12:18:59 +0700

    tambah validasi duplikasi di awal

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -30,5 +30,15 @@ public:
 
         // Step 2: Assign values
         newNode->noMhs = nim;
+
+         // Step 3: Insert at beginning if list empty or first element
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+        }
     }
 };
