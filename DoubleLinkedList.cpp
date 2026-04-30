ef729519319173e0fed77d2cdbf4b101749cf665
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-30 12:17:47 +0700

    tambah fungsi addNode dasar

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -19,4 +19,16 @@ public:
     {
         START = NULL;
     }
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign values
+        newNode->noMhs = nim;
+    }
 };
